02467adbd86d6f29530f82311390dc0a0638fe41
morachake <jacobmoracha@gmail.com>
2023-11-10 00:55:33 +0300

    updates on header and routes

---
+++ src/views/Departments.js
@@ -0,0 +1,11 @@
+import Header from 'components/Headers/Header';
+import React from 'react';
+
+export default function Departments() {
+  return (
+   <div>
+     <Header />
+    <h1>Departments</h1>
+   </div>
+  );
+}
--- src/views/examples/Tables.js
+++ src/views/examples/Tables.js
@@ -17,23 +17,15 @@
 */
 // reactstrap components
 import {
-  Badge,
   Card,
   CardHeader,
   CardFooter,
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledDropdown,
-  DropdownToggle,
-  Media,
   Pagination,
   PaginationItem,
   PaginationLink,
-  Progress,
   Table,
   Container,
   Row,
-  UncontrolledTooltip,
 } from "reactstrap";
 // core components
 import Header from "components/Headers/Header.js";
