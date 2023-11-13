4ff3f77ec596196a95589a1530fa4de24be5e772
morachake <jacobmoracha@gmail.com>
2023-11-13 12:32:45 +0300

    updates

--- src/components/Dashboard/ProjectsTable.js
+++ src/components/Dashboard/ProjectsTable.js
@@ -6,8 +6,11 @@ import {
   Table,
   Row,
 } from "reactstrap";
-
+import projectsData from 'data/projectdata';
 export default function ProjectsTable() {
+  const data = projectsData.map(project =>{
+    
+  })
   return (
     <>
     <CardBody className="shadow">
@@ -46,41 +49,10 @@ export default function ProjectsTable() {
                       <i className="fas fa-arrow-up text-success mr-3" /> 46,53%
                     </td>
                   </tr>
-                  <tr>
-                    <th scope="row">/argon/index.html</th>
-                    <td>3,985</td>
-                    <td>319</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-warning mr-3" />{" "}
-                      46,53%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/charts.html</th>
-                    <td>3,513</td>
-                    <td>294</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-warning mr-3" />{" "}
-                      36,49%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/tables.html</th>
-                    <td>2,050</td>
-                    <td>147</td>
-                    <td>
-                      <i className="fas fa-arrow-up text-success mr-3" /> 50,87%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/profile.html</th>
-                    <td>1,795</td>
-                    <td>190</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-danger mr-3" />{" "}
-                      46,53%
-                    </td>
-                  </tr>
+                 
+                  
+                 
+                 
                 </tbody>
               </Table>
             </CardBody>
---
+++ src/data/projectdata.js
@@ -0,0 +1,77 @@
+const projectsData = [
+    {
+      sector: 'Education',
+      projectName: 'Supply and Delivery of Tools and Equipment for Vocational Training Centres',
+      location: 'Across the County',
+      contractSum: null,
+      budgetAllocation: 5129504,
+      amountPaid: null,
+      contractVariation: null,
+      implementationStatus: null,
+      sourceOfFunding: 'GoK',
+      remarks: null
+    },
+    {
+        sector: 'Transport & Infrastructure',
+        projectName: 'PROPOSED WALKWAYS GAZEBO PLATFORM FOR RAILWAYS ROUND ABOUT PUBLIC PARK CGM/DPW/RR/WG/10',
+        location: 'MVITA WARD',
+        contractSum: null,
+        budgetAllocation: 806069,
+        amountPaid: null,
+        contractVariation: null,
+        implementationStatus: null,
+        sourceOfFunding: 'GoK',
+        remarks: null
+      },
+      {
+        sector: 'Transport & Infrastructure',
+        projectName: 'PERIODIC MAINTENANCE OF MIKINDANI ESTATE (L28)ROADS CGM/PRO/94/2018-2019',
+        location: 'JOMVU WARD',
+        contractSum: null,
+        budgetAllocation: 987670,
+        amountPaid: null,
+        contractVariation: null,
+        implementationStatus: null,
+        sourceOfFunding: 'GoK',
+        remarks: null
+      },
+      {
+        sector: 'Education',
+        projectName: 'BEING PAYMENT OF RETENTION FOR PROPOSED ECDS AT DIGIRIKANI MOMBASA',
+        location: 'KISAUNI WARD',
+        contractSum: null,
+        budgetAllocation: 1562599,
+        amountPaid: null,
+        contractVariation: null,
+        implementationStatus: null,
+        sourceOfFunding: 'GoK',
+        remarks: null
+      },
+      {
+        sector: 'Education',
+        projectName: 'PROPOSED EARLY CHILDHOOD DEVELOPMENT CENTRE ECDE AT CHAANI PRIMARY SCHOOL CGM/PRO/03(04)2014-2015',
+        location: 'CHANGAMWE WARD',
+        contractSum: null,
+        budgetAllocation: 2235177,
+        amountPaid: null,
+        contractVariation: null,
+        implementationStatus: null,
+        sourceOfFunding: 'GoK',
+        remarks: null
+      },
+      {
+        sector: 'Finance- Revenue',
+        projectName: 'REVENUE MANAGEMENT SYSTEM-TECHBIZ LIMITED',
+        location: 'MVITA WARD',
+        contractSum: null,
+        budgetAllocation: 34546779,
+        amountPaid: null,
+        contractVariation: null,
+        implementationStatus: null,
+        sourceOfFunding: 'GoK',
+        remarks: null
+      }
+
+  ];
+  
+  export default projectsData;
