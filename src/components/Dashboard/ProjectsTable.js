cbc2796fbc4f805134aace12352c9b9247a871e1
morachake <jacobmoracha@gmail.com>
2023-11-20 00:38:04 +0300

    UPDATES

--- src/components/Dashboard/ProjectsTable.js
+++ src/components/Dashboard/ProjectsTable.js
@@ -15,7 +15,7 @@ export default function ProjectsTable() {
         // ... other filters
     });
     useEffect(() => {
-        fetch('http://127.0.0.1:5000/forms')
+        fetch('http://127.0.0.1:5000/form')
             .then(response => response.json())
             .then(data => setProjects(data))
             .catch(error => console.error('Error fetching data:', error));
