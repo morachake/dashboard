a87c04b5827768a590cdbdb331e1b94156cb4521
morachake <jacobmoracha@gmail.com>
2023-11-29 12:57:12 +0300

    updates complete

--- src/views/ministry/MinTable.js
+++ src/views/ministry/MinTable.js
@@ -1,130 +1,125 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { InputText } from 'primereact/inputtext';
-import { Dropdown } from 'primereact/dropdown';
-import { Tag } from 'primereact/tag';
-import { FilterMatchMode } from 'primereact/api';
+import { CardHeader, Container,Card, Col ,  CardBody, Row,ListGroup ,ListGroupItem} from 'reactstrap';
 import UserHeader from 'components/Headers/UserHeader';
 import { useAuth } from 'context/AuthContext';
-import {
-    Container,
-    Row,
-    Col,
-    Card,
-    CardBody,
-    Input,
-    Button,
-    ListGroup,
-    ListGroupItem
-} from "reactstrap";
-export default function MinTable() {
-    const { user } = useAuth();
-    const [projects, setProjects] = useState(null);
-    const [globalFilterValue, setGlobalFilterValue] = useState('');
-    const [statuses] = useState(['Stalled', 'Complete', 'Incomplete']);
-    const [expandedRows, setExpandedRows] = useState(null); // Changed to manage expanded rows
 
-    useEffect(() => {
-        fetch('http://127.0.0.1:5000/forms')
-            .then(response => response.json())
-            .then(data => {
-                const userProjects = data.filter(project => project.user_id === user.id);
-                setProjects(userProjects);
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    }, [user.id]);
+export default function ProjectsTable() {
+  const [projectData, setProjects] = useState([]);
+  const [expandedRows, setExpandedRows] = useState(null);
+    const user = useAuth()
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/forms')
+      .then(response => response.ok ? response.json() : Promise.reject('Network response was not ok'))
+      .then(data => { 
+        // const filteredProjects = data.filter(project => project.user_id === user.id);
+        setProjects(data)
+      })
+      .catch(error => console.error('Error fetching projects:', error));
+  }, []);
+  console.log(projectData)
+  const onRowToggle = (e) => {
+    setExpandedRows(e.data);
+  };
 
-    const onGlobalFilterChange = (e) => {
-        const value = e.target.value || '';
-        setGlobalFilterValue(value);
+ 
+
+  const formatCurrency = (value) => {
+    // Check if value is a number or can be converted to one
+    const num = parseFloat(value);
+    return isNaN(num) ? 'N/A' : num.toLocaleString();
+  };
+  const rowExpansionTemplate = (data) => {
+    const processRecommendations = (recommendations) => {
+      return recommendations
+        .split(/\d+\.\s*/)  // Split by the number followed by a period and space
+        .filter(item => item.trim() !== '');  // Filter out empty strings
     };
-
-    const onRowToggle = (e) => {
-        setExpandedRows(e.data);
-    };
-
-    const rowExpansionTemplate = (data) => {
-        return (
-            <Card className="expandedRowContent">
-                <Col>
-                    <Row>
-                        <Col>
-                            <h5>Details for {data.project_name}</h5>
-                            <p>Description: {data.description}</p>
-                            <p>Before Images: <a href={data.before_images} target="_blank" rel="noopener noreferrer">View</a></p>
-                            <p>After Images: <a href={data.after_images} target="_blank" rel="noopener noreferrer">View</a></p>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <p>Recommendations: {data.recommendations}</p>
-                            <p>Remarks: {data.remarks}</p>
-                            <p>Time Frame: {data.time_frame}</p>
-                        </Col>
-                    </Row>
-                </Col>
-            </Card>
-        );
-    };
-
-    const getSeverity = (status) => {
-        switch (status) {
-            case 'Cancelled': return 'danger';
-            case 'Complete': return 'success';
-            case 'Stalled': return 'warning';
-            case 'Incomplete': return 'info';
-            default: return 'secondary';
-        }
-    };
-
-    const statusBodyTemplate = (rowData) => {
-        return <Tag value={rowData.status} severity={getSeverity(rowData.status)} />;
-    };
-
-    const statusRowFilterTemplate = (options) => {
-        return (
-            <Dropdown value={options.value} options={statuses} onChange={(e) => options.filterApplyCallback(e.value)} placeholder="Select a Status" className="p-column-filter" showClear />
-        );
-    };
-
-    const renderHeader = () => {
-        return (
-            <div className="flex justify-content-end">
-                <span className="p-input-icon-left">
-                    <i className="pi pi-search" />
-                    <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
-                </span>
-            </div>
-        );
-    };
-
-    const header = renderHeader();
-
+    const recommendationsList = processRecommendations(data.recommendations);
+  
+    
+  
+    const formattedContractSum = formatCurrency(data.contract_sum);
+  
+    // Render List Items
+    const renderListItems = (items) => items.map((item, index) => <ListGroupItem key={index}>{item}</ListGroupItem>);
+  
+    const certificatesList = data.certificates.map(cert => `${cert.certificate_number}: ${cert.amount_certified}`);
+  
     return (
-        <>
-            <UserHeader />
-            <Container className="mt--7" fluid>
-                <div className="card">
-                    <DataTable value={projects} paginator rows={10} dataKey="id"
-                        globalFilter={globalFilterValue} header={header}
-                        emptyMessage="No projects found."
-                        expandedRows={expandedRows}
-                        onRowToggle={onRowToggle}
-                        rowExpansionTemplate={rowExpansionTemplate}
-                    >
-                        <Column expander style={{ width: '3em' }} />
-                        <Column field="project_name" header="Project Name" filter filterPlaceholder="Search by name" />
-                        <Column field="subcounty" header="Subcounty" filter filterPlaceholder="Search by subcounty" />
-                        <Column field="ward" header="Ward" filter filterPlaceholder="Search by ward" />
-                        <Column field="contract_sum" header="Contract Sum" />
-                        <Column field="contractor_details" header="Contractor Details" />
-                        <Column field="certificate_number" header="Certificate Number" />
-                        <Column field="amount_certified" header="Amount Certified" />
-                        <Column field="status" header="Status" body={statusBodyTemplate} filter filterElement={statusRowFilterTemplate} />
-                    </DataTable>
-                </div>
-            </Container>
-        </>
+      <Card>
+        <CardHeader>
+          <h5>Details for {data.project_name}</h5>
+        </CardHeader>
+        <CardBody>
+          <Row>
+            <Col lg="6" md="12">
+              <p><h4>Description:</h4> {data.description}</p>
+              <p><h4>Contractor Details:</h4> {data.contractor_details}</p>
+              <p><h4>Contract Sum:</h4> {formattedContractSum}</p>
+              <p><h4>Status:</h4> {data.status}</p>
+              <p><h4>Time Frame:</h4> {data.time_frame}</p>
+              <p><h4>Certificates:</h4></p>
+              <ListGroup>
+                {renderListItems(certificatesList)}
+              </ListGroup>
+            </Col>
+            <Col lg="6" md="12">
+              <p><h4>Subcounty:</h4> {data.subcounty}</p>
+              <p><h4>Ward:</h4> {data.ward}</p>
+              <p><h4>Remarks:</h4> {data.remarks}</p>
+              <p><h4>Recommendations:</h4></p>
+              <ListGroup>
+                {renderListItems(recommendationsList)}
+              </ListGroup>
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
     );
+  };
+
+   
+  
+
+  return (
+    <>
+      <UserHeader />
+      <Container className="mt--7" fluid>
+        <CardHeader>
+          <div className="card">
+            {projectData && projectData.length > 0 ? (
+              <DataTable 
+                value={projectData}
+                paginator 
+                rows={10} 
+                dataKey="id"
+                emptyMessage="No projects found."
+                expandedRows={expandedRows}
+                onRowToggle={onRowToggle}
+                rowExpansionTemplate={rowExpansionTemplate}
+              >
+                <Column expander style={{ width: '3em' }} />
+                <Column field="project_name" header="Project Name" />
+                <Column field="status" header="Status" />
+                <Column field="subcounty" header="Subcounty" />
+                <Column field="ward" header="Ward" />
+                <Column field="contract_sum" header="Contract Sum" />
+              </DataTable>
+            ) : (
+            <Card>
+                <CardHeader>
+                No data available
+                </CardHeader>
+                <CardBody>
+                    Please Proceed to add data to your account for it to be visible here
+                </CardBody>
+            </Card>
+            )}
+          </div>
+        </CardHeader>
+      </Container>
+    </>
+  );
 }
