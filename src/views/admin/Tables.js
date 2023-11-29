01361e47f122b94d6399ce3039f8c4bbb4741332
morachake <jacobmoracha@gmail.com>
2023-11-29 12:32:23 +0300

    updating tables

--- src/views/admin/Tables.js
+++ src/views/admin/Tables.js
@@ -1,121 +1,107 @@
 import React, { useEffect, useState } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { InputText } from 'primereact/inputtext';
-import { Dropdown } from 'primereact/dropdown';
-import { Tag } from 'primereact/tag';
-import { CardHeader, Container } from 'reactstrap';
+import { CardHeader, Container,Card, Col ,  CardBody, Row,ListGroup ,ListGroupItem} from 'reactstrap';
 import UserHeader from 'components/Headers/UserHeader';
 
 export default function ProjectsTable() {
-  const [globalFilterValue, setGlobalFilterValue] = useState('');
-  const [statuses] = useState(['Stalled', 'Complete', 'Incomplete', 'Cancelled']);
   const [projectData, setProjects] = useState([]);
-  const [expandedRowId, setExpandedRowId] = useState(null);
+  const [expandedRows, setExpandedRows] = useState(null);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/projects')
+    fetch('http://127.0.0.1:5000/forms')
       .then(response => response.ok ? response.json() : Promise.reject('Network response was not ok'))
-      .then(data => Array.isArray(data) ? setProjects(data) : console.error('Data is not an array:', data))
+      .then(data => setProjects(data))
       .catch(error => console.error('Error fetching projects:', error));
   }, []);
-
-  const onGlobalFilterChange = (e) => {
-    setGlobalFilterValue(e.target.value || '');
-  };
-
+  console.log(projectData)
   const onRowToggle = (e) => {
-    const currentRowId = e.data.projectId;
-    setExpandedRowId(currentRowId === expandedRowId ? null : currentRowId);
+    setExpandedRows(e.data);
   };
+
+ 
 
   const rowExpansionTemplate = (data) => {
+    const processRecommendations = (recommendations) => {
+      return recommendations
+        .split(/\d+\.\s*/)  // Split by the number followed by a period and space
+        .filter(item => item.trim() !== '');  // Filter out empty strings
+    };
+  
+    // Process the recommendations string
+    const recommendationsList = processRecommendations(data.recommendations);
+  
+    // Render List Items
+    const renderListItems = (items) => items.map((item, index) => <ListGroupItem key={index}>{item}</ListGroupItem>);
+  
+    const certificatesList = data.certificates.map(cert => `${cert.certificate_number}: ${cert.amount_certified}`);
+  
     return (
-      <div className="p-grid">
-        <div className="p-col-12">
-          <h5>Details for {data.projectName}</h5>
-          {/* Add more details about the project here */}
-        </div>
-      </div>
+      <Card>
+        <CardHeader>
+          <h5>Details for {data.project_name}</h5>
+        </CardHeader>
+        <CardBody>
+          <Row>
+            <Col lg="6" md="12">
+              <p><h4>Description:</h4> {data.description}</p>
+              <p><h4>Contractor Details:</h4> {data.contractor_details}</p>
+              <p><h4>Contract Sum:</h4> {data.contract_sum}</p>
+              <p><h4>Status:</h4> {data.status}</p>
+              <p><h4>Time Frame:</h4> {data.time_frame}</p>
+              <p><h4>Certificates:</h4></p>
+              <ListGroup>
+                {renderListItems(certificatesList)}
+              </ListGroup>
+            </Col>
+            <Col lg="6" md="12">
+              <p><h4>Subcounty:</h4> {data.subcounty}</p>
+              <p><h4>Ward:</h4> {data.ward}</p>
+              <p><h4>Remarks:</h4> {data.remarks}</p>
+              <p><h4>Recommendations:</h4></p>
+              <ListGroup>
+                {renderListItems(recommendationsList)}
+              </ListGroup>
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
     );
   };
 
-  const getSeverity = (status) => {
-    switch (status) {
-      case 'Complete': return 'success';
-      case 'Stalled': return 'danger';
-      case 'Incomplete': return 'info';
-      default: return 'secondary';
-    }
-  };
-
-  const renderHeader = () => {
-    return (
-      <div className="flex justify-content-end">
-        <span className="p-input-icon-left">
-          <i className="pi pi-search" />
-          <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
-        </span>
-      </div>
-    );
-  };
-
-  const statusBodyTemplate = (rowData) => <Tag value={rowData.status} severity={getSeverity(rowData.status)} />;
-  const statusRowFilterTemplate = (options) => (
-    <Dropdown value={options.value} options={statuses} onChange={(e) => options.filterApplyCallback(e.value)} placeholder="Select a Status" className="p-column-filter" showClear />
-  );
-
-  const budgetAllocationBodyTemplate = (rowData) => {
-    return !rowData || rowData.budgetAllocation == null ? <span>Unknown</span> : <span>{rowData.budgetAllocation.toLocaleString()}</span>;
-  };
-
-  const projectNameBodyTemplate = (rowData) => {
-    if (!rowData || !rowData.projectName) {
-      return <span>Unknown Project</span>;
-    }
-    const trimmedName = rowData.projectName.length > 25 ? `${rowData.projectName.substring(0, 25)}...` : rowData.projectName;
-    return <span title={rowData.projectName}>{trimmedName}</span>;
-  };
-
-  const getExpandedRows = () => {
-    if (expandedRowId == null) return null;
-    const expandedRow = projectData.find(row => row.projectId === expandedRowId);
-    return expandedRow ? [expandedRow] : null;
-  };
+   
+  
 
   return (
     <>
-    <UserHeader />
-    <Container className="mt--7" fluid>
-      <CardHeader>
-        <div className="card">
-          {projectData && projectData.length > 0 ? (
-            <DataTable 
-              value={projectData}
-              paginator 
-              rows={10} 
-              dataKey="projectId" 
-              globalFilter={globalFilterValue}
-              emptyMessage="No projects found."
-              expandedRows={expandedRowId ? projectData.filter(row => row.projectId === expandedRowId) : null}
-              onRowToggle={onRowToggle}
-              rowExpansionTemplate={rowExpansionTemplate}
-            >
-              <Column expander style={{ width: '3em' }} />
-              <Column field="projectName" header="Project Name" body={projectNameBodyTemplate} filter filterPlaceholder="Search by name" />
-              <Column field="location" header="Location" filter filterPlaceholder="Search by location" />
-              <Column field="budgetAllocation" header="Budget Allocation" body={budgetAllocationBodyTemplate} />
-              <Column field="sector" header="Sector" />
-              <Column field="sourceOfFunding" header="Source of Funding" />
-              <Column field="status" header="Status" body={statusBodyTemplate} filter filterElement={statusRowFilterTemplate} />
-            </DataTable>
-          ) : (
-            <div>No data available</div>
-          )}
-        </div>
-      </CardHeader>
-    </Container>
-  </>
-
+      <UserHeader />
+      <Container className="mt--7" fluid>
+        <CardHeader>
+          <div className="card">
+            {projectData && projectData.length > 0 ? (
+              <DataTable 
+                value={projectData}
+                paginator 
+                rows={10} 
+                dataKey="id"
+                emptyMessage="No projects found."
+                expandedRows={expandedRows}
+                onRowToggle={onRowToggle}
+                rowExpansionTemplate={rowExpansionTemplate}
+              >
+                <Column expander style={{ width: '3em' }} />
+                <Column field="project_name" header="Project Name" />
+                <Column field="status" header="Status" />
+                <Column field="subcounty" header="Subcounty" />
+                <Column field="ward" header="Ward" />
+                <Column field="contract_sum" header="Contract Sum" />
+              </DataTable>
+            ) : (
+              <div>No data available</div>
+            )}
+          </div>
+        </CardHeader>
+      </Container>
+    </>
   );
 }
