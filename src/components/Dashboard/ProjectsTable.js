3f63d9b1f090b8d24c514fe10079892bfc11503e
morachake <jacobmoracha@gmail.com>
2023-12-05 22:57:02 +0300

    cleaning code

--- src/components/Dashboard/ProjectsTable.js
+++ src/components/Dashboard/ProjectsTable.js
@@ -1,29 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { InputText } from 'primereact/inputtext';
 import { Tag } from 'primereact/tag';
-import { useNavigate } from 'react-router-dom';
 import { CardHeader } from 'reactstrap';
 
 export default function ProjectsTable({ projectData }) {
-    const [globalFilterValue, setGlobalFilterValue] = useState('');
-
-    const onGlobalFilterChange = (e) => {
-        const value = e.target.value || '';
-        setGlobalFilterValue(value);
-    };
-
-    const renderHeader = () => {
-        return (
-            <div className="flex justify-content-end">
-                <span className="p-input-icon-left">
-                    <i className="pi pi-search" />
-                    <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
-                </span>
-            </div>
-        );
-    };
 
     const statusBodyTemplate = (rowData) => {
         return <Tag value={rowData.status} severity={getSeverity(rowData.status)} />;
@@ -50,18 +31,6 @@ export default function ProjectsTable({ projectData }) {
         const trimmedName = rowData.project_name.length > 25 ? `${rowData.project_name.substring(0, 25)}...` : rowData.project_name;
         return <span title={rowData.project_name}>{trimmedName}</span>;
     };
-
-    const navigate = useNavigate();
-
-    const onRowClick = (event) => {
-        const rowData = event.data;
-        if (rowData.id) {
-            navigate(`/admin/project/${rowData.id}`);
-        } else {
-            console.error('The row data does not have an id property:', rowData);
-        }
-    };
-
     function shortDesc(text) {
         return text.length > 50 ? `${text.slice(0, 50)}...` : text;
     }
@@ -80,7 +49,7 @@ export default function ProjectsTable({ projectData }) {
         <CardHeader>
             <div className="card">
                 {projectData && projectData.length > 0 ? (
-                    <DataTable value={projectData} paginator rows={10} dataKey="id"  onRowClick={onRowClick} emptyMessage="No projects found.">
+                    <DataTable value={projectData} paginator rows={10} dataKey="id"   emptyMessage="No projects found.">
                         <Column field="project_name" header="Project Name" body={projectNameBodyTemplate} />
                         <Column field="description" header="Description" body={descriptionWithTooltip} />
                         {/* <Column field="sector" header="Sector" /> */}
